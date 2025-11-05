100abf5ca67b0a33ce3612ee672531ede104c21f
VTN411 <nghiavotrong411@gmail.com>
2025-11-06 01:42:06 +0700

    feat: Initial commit for Spring Boot Backend (Auth & Security)

---
+++ backend/src/main/java/stu/kho/backend/BackendApplication.java
@@ -0,0 +1,22 @@
+package stu.kho.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+}
